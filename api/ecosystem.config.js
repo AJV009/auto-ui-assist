0866d4d28f3ba747cdc36c60edfb0847583dba59
Alphons Jaimon <jaimonalphons@gmail.com>
2024-06-14 05:15:59 -0700

    chore: Disable watch mode for API in ecosystem.config.js

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "AutoUIAssist API",
         script: "source autoui/bin/activate; uvicorn app:app --host 0.0.0.0 --port 8000",
-        watch: true,
+        watch: false,
         max_memory_restart: "1000M",
         autorestart: true,
         max_restarts: 10,
